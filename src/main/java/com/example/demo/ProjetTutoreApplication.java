385f94593dc3be3f6e38d0f8367fc12544567cb5
Soufiane AMMY DRISS <ammydriss18@gmail.com>
2020-03-29 21:48:33 +0100

    initial

---
+++ src/main/java/com/example/demo/ProjetTutoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTutoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetTutoreApplication.class, args);
+	}
+
+}
