6dbcd0b27d53cf972c1c6ea572bb354ff71aefac
Soufiane AMMY DRISS <ammydriss18@gmail.com>
2020-04-14 19:48:57 +0100

    swagger

--- src/main/java/com/esisa/back/office/ProjetTutoreApplication.java
+++ src/main/java/com/esisa/back/office/ProjetTutoreApplication.java
@@ -3,7 +3,10 @@ package com.esisa.back.office;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ProjetTutoreApplication {
 
 	public static void main(String[] args) {
