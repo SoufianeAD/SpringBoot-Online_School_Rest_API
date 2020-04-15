a82bac029e146221415ac8679bec455750b0a3bf
Soufiane AMMY DRISS <ammydriss18@gmail.com>
2020-04-16 00:08:24 +0100

    File+fileController

---
+++ src/main/java/com/esisa/back/office/controllers/FileController.java
@@ -0,0 +1,58 @@
+package com.esisa.back.office.controllers;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.esisa.back.office.entities.File;
+import com.esisa.back.office.repositories.FileRepository;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+	
+	@Autowired
+	private FileRepository fileRepository;
+	
+	@PostMapping("/add")
+	public File add(@RequestBody File file) {
+		return fileRepository.save(file);
+	}
+	
+	@PostMapping("/upload")
+	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
+		//service upload
+		return new ResponseEntity<>("File upload ok!", HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public File update(@RequestBody File file) {
+		//service update
+		return fileRepository.save(file);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable("id") ObjectId id) {
+		//service delete
+		fileRepository.deleteById(id);
+	}
+	
+	@GetMapping("/getById/{id}")
+	public Optional<File> getById(@PathVariable("id") ObjectId id) {
+		return fileRepository.findById(id);
+	}
+
+}
--- src/main/java/com/esisa/back/office/entities/File.java
+++ src/main/java/com/esisa/back/office/entities/File.java
@@ -1,5 +1,56 @@
 package com.esisa.back.office.entities;
 
-public class File {
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
+public class File {
+	
+	@Id
+	private ObjectId id;
+	private String title;
+	private String file;//path and file name
+	
+	public File() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public File(String title, String file) {
+		super();
+		this.title = title;
+		this.file = file;
+	}
+
+	public File(ObjectId id, String title, String file) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.file = file;
+	}
+
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+	
 }
