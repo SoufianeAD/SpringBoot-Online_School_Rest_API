5bb74783a9dce7abb0d90e1d8ade02ecc59dc380
Soufiane AMMY DRISS <ammydriss18@gmail.com>
2020-04-16 18:11:32 +0100

    @crossOrigin

--- src/main/java/com/esisa/back/office/controllers/AccountController.java
+++ src/main/java/com/esisa/back/office/controllers/AccountController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.esisa.back.office.entities.Account;
 import com.esisa.back.office.repositories.AccountRepository;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/account")
 public class AccountController {
 
--- src/main/java/com/esisa/back/office/controllers/SchoolController.java
+++ src/main/java/com/esisa/back/office/controllers/SchoolController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.esisa.back.office.entities.School;
 import com.esisa.back.office.repositories.SchoolRepository;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/school")
 public class SchoolController {
 
